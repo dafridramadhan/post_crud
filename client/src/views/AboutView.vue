da9a4fd402cd064183e127b0fe3255f7f83abd70
unknown <dafridramadhan@gmail.com>
2023-03-02 14:12:00 +0700

    First Commit

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h1>About Application</h1>
+    <p>Stack : MEVN (MongoDB, Express, Vue, Nodejs)</p>
+    <h1>Back-end</h1>
+    <p>- Node js</p>
+    <p>- Express js</p>
+    <p>- MongoDB</p>
+    <h1>Front-end</h1>
+    <p>- Vue js</p>
+    <p>- Vue router</p>
+    <p>- Axios</p>
+  </v-container>
+</template>
